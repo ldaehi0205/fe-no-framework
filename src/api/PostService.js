a34654cc981a0d5181eb15d370a88bf71c0f0375
ldaehi0205 <ldaehi0205@gmail.com>
2026-03-07 16:01:27 +0900

    mockup데이터 추가

--- src/api/PostService.js
+++ src/api/PostService.js
@@ -5,8 +5,16 @@ class PostService extends HttpClient {
     super();
   }
 
-  getPosts() {
-    return super.get('/posts.json');
+  async getPosts(category) {
+    const res = await super.get('/posts.json');
+    // mock data를 활용하여 카테고리별로 포스트를 필터링
+    if (category === 'latest') {
+      return res;
+    } else if (category) {
+      return { posts: res.posts.filter(post => post.category === category) };
+    } else {
+      return res;
+    }
   }
 
   getPostDetail(id) {
